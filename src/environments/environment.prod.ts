ed6f4d127efa4508a0b06c9062622e4281062007
mnauman319 <mnauman319@gmail.com>
2020-09-13 13:44:51 -0500

    changed prod url to matche ec2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: "https://ec2-18-216-111-186.us-east-2.compute.amazonaws.com:8080",
+  baseUrl: "https://ec2-3-21-171-101.us-east-2.compute.amazonaws.com:8080",
 };
